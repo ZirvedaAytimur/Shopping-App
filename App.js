4aa037375544cea9ceb6a091ecf675720b42e53b
ZirvedaAytimur <zirvedaytimur@gmail.com>
2019-08-30 23:30:33 +0300

    Added items to the cart

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
-import * as Font from 'expo-font'
+import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer
 });
 
 const store = createStore(rootReducer);
