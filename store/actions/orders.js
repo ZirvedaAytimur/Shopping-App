d85f6925756a7909dce24b7af816d614388a9205
ZirvedaAytimur <zirvedaytimur@gmail.com>
2019-09-04 22:19:47 +0300

    Stored orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,34 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+  return async dispatch => {
+    const date = new Date();
+    const responce = await fetch('https://shop-app-952c9.firebaseio.com/orders/u1.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        cartItems,
+        totalAmount,
+        date: date.toISOString()
+      })
+    });
+
+    if (!responce.ok) {
+      throw new Error('Something went wrong.');
+    }
+
+    const resData = await responce.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date
+      }
+    });
   };
 };
