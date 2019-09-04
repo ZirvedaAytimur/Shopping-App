b337b7fb99ffcfb92cfff1db84a0a50855d2899c
ZirvedaAytimur <zirvedaytimur@gmail.com>
2019-09-04 22:59:11 +0300

    Fetched stored orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,25 +1,62 @@
+import Order from '../../models/order';
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch(
+        'https://shop-app-952c9.firebaseio.com/orders/u1.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+      dispatch({ type: SET_ORDERS, orders: loadedOrders });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
     const date = new Date();
-    const responce = await fetch('https://shop-app-952c9.firebaseio.com/orders/u1.json', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        cartItems,
-        totalAmount,
-        date: date.toISOString()
-      })
-    });
+    const response = await fetch(
+      'https://shop-app-952c9.firebaseio.com/orders/u1.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString()
+        })
+      }
+    );
 
-    if (!responce.ok) {
-      throw new Error('Something went wrong.');
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
     }
 
-    const resData = await responce.json();
+    const resData = await response.json();
 
     dispatch({
       type: ADD_ORDER,
