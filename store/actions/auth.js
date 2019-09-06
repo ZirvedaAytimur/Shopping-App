0ca3dee34f7ef2e26d05375c54a21fbb40a9fc7c
ZirvedaAytimur <zirvedaytimur@gmail.com>
2019-09-06 13:43:22 +0300

    Logged the user in

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
     return async dispatch => {
@@ -26,6 +27,36 @@ export const signup = (email, password) => {
 
         dispatch({
             type: SIGNUP
+        });
+    };
+};
+
+export const login = (email, password) => {
+    return async dispatch => {
+        const response = await fetch(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB1F5TOwJ2edCLh68H_RYRgWyQUmVmK2fg',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    returnSecureToken: true
+                })
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+
+        const resData = await response.json();
+        console.log(resData);
+
+        dispatch({
+            type: LOGIN
         });
     };
 };
